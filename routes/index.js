ab7cb6a962592885a6aec31a49d7424ceb025caf
Randolph Bailey <randolphbailey@gmail.com>
2019-07-05 20:32:20 -0500

    add additional authenticaton

--- routes/index.js
+++ routes/index.js
@@ -36,24 +36,28 @@ module.exports = function(app) {
   );
 
   //Update DB with file upload info sent from front-end
-  app.put("/upload/fileInfo", (req, res) => {
-    db.Video.update(
-      {
-        b2AccountId: req.body.accountId,
-        b2Action: req.body.action,
-        b2BucketId: req.body.bucketId,
-        b2ContentLength: req.body.contentLength,
-        b2ContentSHA1: req.body.contentSha1,
-        b2ContentType: req.body.contentType,
-        b2FileId: req.body.fileId,
-        b2FileInfo: req.body.fileInfo,
-        b2UploadTimestamp: req.body.uploadTimestamp,
-        videoURL: "https://v.videopsi.com/file/videopsi/" + req.body.fileName,
-        b2FileName: req.body.fileName,
-        title: req.body.fileInfo.title,
-        description: req.body.fileInfo.description
-      },
-      { where: { vId: req.body.fileInfo.vid } }
-    ).then(rowsUpdated => res.json(rowsUpdated));
-  });
+  app.put(
+    "/upload/fileInfo",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+      db.Video.update(
+        {
+          b2AccountId: req.body.accountId,
+          b2Action: req.body.action,
+          b2BucketId: req.body.bucketId,
+          b2ContentLength: req.body.contentLength,
+          b2ContentSHA1: req.body.contentSha1,
+          b2ContentType: req.body.contentType,
+          b2FileId: req.body.fileId,
+          b2FileInfo: req.body.fileInfo,
+          b2UploadTimestamp: req.body.uploadTimestamp,
+          videoURL: "https://v.videopsi.com/file/videopsi/" + req.body.fileName,
+          b2FileName: req.body.fileName,
+          title: req.body.fileInfo.title,
+          description: req.body.fileInfo.description
+        },
+        { where: { vId: req.body.fileInfo.vid } }
+      ).then(rowsUpdated => res.json(rowsUpdated));
+    }
+  );
 };
