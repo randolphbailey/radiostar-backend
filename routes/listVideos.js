35f71b6732d6ca72f9cea00051a11c1f74fe809e
Randolph Bailey <randolphbailey@gmail.com>
2019-07-29 17:59:55 -0500

    limit attributes returned in video list

--- routes/listVideos.js
+++ routes/listVideos.js
@@ -2,7 +2,10 @@ const db = require("../models");
 
 module.exports = function(app) {
   app.get("/videolist", (req, res) => {
-    db.Video.findAll({ limit: 10 })
+    db.Video.findAll({
+      limit: 10,
+      attributes: [title, vId, description, videoURL]
+    })
       .then(videos => res.json(videos))
       .catch(err => console.log("Error fetching videos:", err));
   });
