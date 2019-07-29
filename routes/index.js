1fbf17c9d78db228f9c28e7d979098fa44a703c3
Randolph Bailey <randolphbailey@gmail.com>
2019-07-29 15:16:28 -0500

    tweak B2 authorization

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ async function GetBucket() {
   try {
     await b2.authorize(); // must authorize first
     let response = await b2.getBucket({ bucketName: "videopsi" });
-    console.log(response.data);
+    console.log("B2 Authorized");
   } catch (err) {
     console.log("Error getting bucket:", err);
   }
