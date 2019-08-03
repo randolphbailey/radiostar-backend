9104b944b9dbe48feee07aa10cc1df294dbde450
Randolph Bailey <randolphbailey@gmail.com>
2019-08-03 18:08:32 -0500

    app now fetches users videos

--- routes/listVideos.js
+++ routes/listVideos.js
@@ -1,12 +1,17 @@
 const db = require("../models");
 
 module.exports = function(app) {
-  app.get("/videolist", (req, res) => {
-    db.Video.findAll({
-      limit: 10,
-      attributes: ["title", "vId", "description", "videoURL"]
-    })
-      .then(videos => res.json(videos))
-      .catch(err => console.log("Error fetching videos:", err));
-  });
+  app.get(
+    "/videolist",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+      db.Video.findAll({
+        limit: 10,
+        attributes: ["title", "vId", "description", "videoURL"],
+        where: { UserId: req.user.id }
+      })
+        .then(videos => res.json(videos))
+        .catch(err => console.log("Error fetching videos:", err));
+    }
+  );
 };
